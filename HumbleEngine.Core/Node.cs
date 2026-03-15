d41d3a26ee2c8663cf6c865004155f95acc97f6c
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-03-15 10:23:59 +0100

    Basic implementation for parents and children of a Node.

--- HumbleEngine.Core/Node.cs
+++ HumbleEngine.Core/Node.cs
@@ -2,5 +2,23 @@ namespace HumbleEngine.Core;
 
 public class Node
 {
+    public Node? Parent { get; private set; }
+    public List<Node> Children { get; private set; } = [];
+    
+    public void AddChild(Node child)
+    {
+        if (!Children.Contains(child))
+        {
+            Children.Add(child);
+        }    
+    }
+
+    public void RemoveChild(Node child)
+    {
+        if (Children.Contains(child))
+        {
+            Children.Remove(child);
+        }
+    }
     
 }
