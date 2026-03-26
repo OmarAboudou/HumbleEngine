ecd485e521a80ca3fbc01b4f88b4e6c7052d41ef
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-03-26 13:39:21 +0100

    Added Processing and PhysicsProcessing for NodeTree

--- HumbleEngine.Core/NodeTree.cs
+++ HumbleEngine.Core/NodeTree.cs
@@ -9,5 +9,19 @@ public class NodeTree
         Root = root;
         root.Tree = this;
     }
-    
+
+    public void Process(double delta) => ExecuteInPrefixOrder(node => node.Process(delta));
+    public void PhysicsProcess(double delta) => ExecuteInPrefixOrder(node => node.PhysicsProcess(delta));
+
+    private void ExecuteInPrefixOrder(Action<Node> action)
+    {
+        List<Node> nodesInPrefixOrder = [Root];
+        while (nodesInPrefixOrder.Count > 0)
+        {
+            Node current = nodesInPrefixOrder[0];
+            action(current);
+            nodesInPrefixOrder.RemoveAt(0);
+            nodesInPrefixOrder.AddRange(current.Children);
+        }
+    }
 }
