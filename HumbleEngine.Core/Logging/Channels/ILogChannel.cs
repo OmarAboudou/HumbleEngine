969696620b179d385398d45be2a26fc38cf28d3c
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-03-30 14:17:37 +0200

    Added XML Doc

--- HumbleEngine.Core/Logging/Channels/ILogChannel.cs
+++ HumbleEngine.Core/Logging/Channels/ILogChannel.cs
@@ -1,6 +1,22 @@
 namespace HumbleEngine.Core;
 
+/// <summary>
+/// Represents a named logging channel used to categorize log entries by system or subsystem.
+/// </summary>
+/// <remarks>
+/// Implement this interface to define a custom channel.
+/// Channels allow filtering log output per subsystem independently of the global log level.
+/// <code>
+/// class PhysicsChannel : ILogChannel
+/// {
+///     public static string ChannelName => "Physics";
+/// }
+/// </code>
+/// </remarks>
 public interface ILogChannel
 {
+    /// <summary>
+    /// The display name of this channel, used when formatting log output.
+    /// </summary>
     public static abstract string ChannelName { get; }
 }
