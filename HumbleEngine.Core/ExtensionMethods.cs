322df8f5ade1a485253800717417a6c89e3170cd
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-03-31 02:55:54 +0200

    Small changes

--- HumbleEngine.Core/ExtensionMethods.cs
+++ HumbleEngine.Core/ExtensionMethods.cs
@@ -36,52 +36,5 @@ public static class ExtensionMethods
     
     #endregion
 
-    /*#region Emitting
-
-    public static void EmitSignal(this object owner, Signal<Action> signal)
-    {
-        if(EnsureEmittingIsAllowed(owner, signal)) signal.Connections.ForEach(connection => connection.Delegate.Invoke());
-    }
-
-    public static void EmitSignal<T1>(this object owner, Signal<Action<T1>> signal, T1 arg1)
-    {
-        if(EnsureEmittingIsAllowed(owner, signal)) signal.Connections.ForEach(connection => connection.Delegate.Invoke(arg1));
-    }
-    
-    public static void EmitSignal<T1, T2>(this object owner, Signal<Action<T1, T2>> signal, T1 arg1, T2 arg2)
-    {
-        if(EnsureEmittingIsAllowed(owner, signal)) signal.Connections.ForEach(connection => connection.Delegate.Invoke(arg1, arg2));
-    }
-    
-    public static void EmitSignal<T1, T2, T3>(this object owner, Signal<Action<T1, T2, T3>> signal, T1 arg1, T2 arg2, T3 arg3)
-    {
-        if(EnsureEmittingIsAllowed(owner, signal)) signal.Connections.ForEach(connection => connection.Delegate.Invoke(arg1, arg2, arg3));
-    }
-    
-    public static void EmitSignal<T1, T2, T3, T4>(this object owner, Signal<Action<T1, T2, T3, T4>> signal, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
-    {
-        if(EnsureEmittingIsAllowed(owner, signal)) signal.Connections.ForEach(connection => connection.Delegate.Invoke(arg1, arg2, arg3, arg4));
-    }
-    
-    #endregion*/
-
-    /*
-    #region Life Cycle
-
-    public static SignalConnection<TDelegate> ConnectToSignal<TDelegate>(
-        this object target,
-        Signal<TDelegate> signal,
-        TDelegate @delegate
-    )
-        where TDelegate : Delegate
-        => signal.Connect(target, @delegate);
-
-    public static void DisconnectFromSignal<TDelegate>(this object target, Signal<TDelegate> signal, SignalConnection<TDelegate> connection)
-        where  TDelegate : Delegate
-    => signal.Disconnect(target, connection);
-
-    #endregion
-    */
-
     #endregion
 }
