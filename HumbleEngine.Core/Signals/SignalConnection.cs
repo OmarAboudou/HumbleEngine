bdde77cd8a7fcb04cb17d0b7ba16514128ca4487
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-03-31 22:59:45 +0200

    Updated Signal system to make it more convenient for users

--- HumbleEngine.Core/Signals/SignalConnection.cs
+++ HumbleEngine.Core/Signals/SignalConnection.cs
@@ -1,3 +1,8 @@
 namespace HumbleEngine.Core;
 
-public readonly record struct SignalConnection<TDelegate>(TDelegate Delegate);
+public readonly record struct SignalConnection<TDelegate>
+    where TDelegate : Delegate
+{
+    public readonly TDelegate Delegate;
+    internal SignalConnection(TDelegate @delegate) => Delegate = @delegate;
+}
