ba6d6d3ff0f1b08273b4022f8759635e9bb7f8ea
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-04-01 00:27:31 +0200

    Added doc

--- HumbleEngine.Core/Signals/SignalingChannel.cs
+++ HumbleEngine.Core/Signals/SignalingChannel.cs
@@ -1,5 +1,8 @@
 namespace HumbleEngine.Core;
 
+/// <summary>
+/// The built-in channel used by HumbleEngine for internal signal system log messages.
+/// </summary>
 internal struct SignalingChannel : ILogChannel
 {
     public static string ChannelName => "SIGNALING";
