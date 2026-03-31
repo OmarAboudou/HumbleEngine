873ce3378f032b1660918d8b291044dc9fd005b5
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-03-31 22:09:18 +0200

    Removed SignalSource and used extension methods Emit instead for a more natural usage by the user.

--- HumbleEngine.Core/ExtensionMethods.cs
+++ HumbleEngine.Core/ExtensionMethods.cs
@@ -19,22 +19,63 @@ public static class ExtensionMethods
 
     #region Creation
     
-    public static SignalSource CreateSignal(this object owner, string name) 
-        => new(new(owner, name));
+    public static Signal<Action> CreateSignal(this object owner, string name) 
+        => new(owner, name);
 
-    public static SignalSource<T1> CreateSignal<T1>(this object owner, string name, string? arg1Name)
-        => new(new(owner, name, (typeof(T1), arg1Name)));
+    public static Signal<Action<T1>> CreateSignal<T1>(this object owner, string name, string? arg1Name)
+        => new(owner, name, (typeof(T1), arg1Name));
     
-    public static SignalSource<T1, T2> CreateSignal<T1, T2>(this object owner, string name, string? arg1Name, string? arg2Name)
-        => new(new(owner, name, (typeof(T1), arg1Name), (typeof(T2), arg2Name)));
+    public static Signal<Action<T1, T2>> CreateSignal<T1, T2>(this object owner, string name, string? arg1Name, string? arg2Name)
+        => new(owner, name, (typeof(T1), arg1Name), (typeof(T2), arg2Name));
 
-    public static SignalSource<T1, T2, T3> CreateSignal<T1, T2, T3>(this object owner, string name, string? arg1Name, string? arg2Name, string? arg3Name)
-        => new(new(owner, name, (typeof(T1), arg1Name), (typeof(T2), arg2Name), (typeof(T3), arg3Name)));
-    
-    public static SignalSource<T1, T2, T3, T4> CreateSignal<T1, T2, T3, T4>(this object owner, string name, string? arg1Name, string? arg2Name, string? arg3Name, string? arg4Name)
-        => new(new(owner, name, (typeof(T1), arg1Name), (typeof(T2), arg2Name), (typeof(T3), arg3Name), (typeof(T4), arg4Name)));
+    public static Signal<Action<T1, T2, T3>> CreateSignal<T1, T2, T3>(this object owner, string name, string? arg1Name, string? arg2Name, string? arg3Name)
+        => new(owner, name, (typeof(T1), arg1Name), (typeof(T2), arg2Name), (typeof(T3), arg3Name));
+
+    public static Signal<Action<T1, T2, T3, T4>> CreateSignal<T1, T2, T3, T4>(this object owner, string name, string? arg1Name, string? arg2Name, string? arg3Name, string? arg4Name)
+        => new(owner, name, (typeof(T1), arg1Name), (typeof(T2), arg2Name), (typeof(T3), arg3Name), (typeof(T4), arg4Name));
     
     #endregion
 
+    #region Emitting
+
+    public static void Emit(this object owner, Signal<Action> signal)
+    {
+        ValidateEmittingPermission(owner, signal);
+        signal.Connections.ForEach(conn => conn.Delegate.Invoke());
+    }
+
+    public static void Emit<T1>(this object owner, Signal<Action<T1>> signal, T1 arg1)
+    {
+        ValidateEmittingPermission(owner, signal);
+        signal.Connections.ForEach(conn => conn.Delegate.Invoke(arg1));
+    }
+
+    public static void Emit<T1, T2>(this object owner, Signal<Action<T1, T2>> signal, T1 arg1, T2 arg2)
+    {
+        ValidateEmittingPermission(owner, signal);
+        signal.Connections.ForEach(conn => conn.Delegate.Invoke(arg1, arg2));
+    }
+
+    public static void Emit<T1, T2, T3>(this object owner, Signal<Action<T1, T2, T3>> signal, T1 arg1, T2 arg2, T3 arg3)
+    {
+        ValidateEmittingPermission(owner, signal);
+        signal.Connections.ForEach(conn => conn.Delegate.Invoke(arg1, arg2, arg3));
+    }
+
+    public static void Emit<T1, T2, T3, T4>(this object owner, Signal<Action<T1, T2, T3, T4>> signal, T1 arg1, T2 arg2,
+        T3 arg3, T4 arg4)
+    {
+        ValidateEmittingPermission(owner, signal);
+        signal.Connections.ForEach(conn => conn.Delegate.Invoke(arg1, arg2, arg3, arg4));
+    }
+    
+    #endregion
+
+    private static void ValidateEmittingPermission<TDelegate>(object owner, Signal<TDelegate> signal)
+        where TDelegate : Delegate
+    {
+        if (signal.Owner != owner) throw new InvalidOperationException($"Only the owner of a signal can emit it.");
+    }
+    
     #endregion
 }
