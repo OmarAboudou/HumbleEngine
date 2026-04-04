a004cf8e9c8e063f62d6dde0cca1f388eca12681
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-04-04 13:41:51 +0200

    Updated Services giving it methods to get services more conveniently.

--- HumbleEngine.Core/Services.cs
+++ HumbleEngine.Core/Services.cs
@@ -5,8 +5,36 @@ namespace HumbleEngine.Core;
 /// </summary>
 public static class Services
 {
+    private static Dictionary<Type, object> _services = [];
+    
     /// <summary>
     /// The shared <see cref="Logger"/> instance used throughout the engine.
     /// </summary>
     public static Logger Logger { get; } = new();
+    
+    public static HumbleTypeRegistry HumbleTypeRegistry { get; } = new();
+
+    static Services()
+    {
+        AddService(Logger);
+        AddService(HumbleTypeRegistry);
+    }
+
+    public static T? Get<T>() => GetAll<T>().FirstOrDefault();
+
+    public static IEnumerable<T> GetAll<T>() => _services.Values.OfType<T>();
+
+    private static void AddService<T>(T service)
+    {
+        if(service == null)
+            throw new ArgumentNullException(nameof(service));
+
+        if (_services.ContainsKey(typeof(T)))
+        {
+            throw new InvalidOperationException($"{typeof(T)} already registered");
+        }
+        
+        _services[typeof(T)] = service;
+    }
+
 }
