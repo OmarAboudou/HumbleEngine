a6f84181000ded0a29c83de3176edfa9ac606517
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-04-04 14:53:57 +0200

    Done V1.

--- HumbleEngine.Core/Serialization/types/HumbleType.cs
+++ HumbleEngine.Core/Serialization/types/HumbleType.cs
@@ -1,31 +1,7 @@
 namespace HumbleEngine.Core;
 
-public class HumbleType
+public readonly record struct HumbleType(Guid Id)
 {
-    public Guid Id { get; }
-    
-    private Type? _runtimeType;
-    public Type RuntimeType
-    {
-        get
-        {
-            _runtimeType ??= Resolve();
-            if(_runtimeType == null) 
-                throw new InvalidOperationException($"Could not resolve type for id {Id}");
-            
-            return _runtimeType;
-        }
-    }
-
-    private readonly HumbleTypeRegistry _registry;
-
-    public HumbleType(Guid id) : this(id, Services.HumbleTypeRegistry) { }
-    public HumbleType(Guid id, HumbleTypeRegistry registry)
-    {
-        Id = id;
-        _registry = registry;
-    }
-
-    public Type Resolve() => _registry.Resolve(Id);
+    public Type Resolve() => Services.HumbleTypeRegistry.Resolve(Id);
 
 }
