9c0793211cd146e16bad32c23e271016dc2bc8a2
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-04-04 15:04:14 +0200

    Added XML doc and Log message.

--- HumbleEngine.Core/Serialization/types/HumbleTypeChannel.cs
+++ HumbleEngine.Core/Serialization/types/HumbleTypeChannel.cs
@@ -1,5 +1,8 @@
 namespace HumbleEngine.Core;
 
+/// <summary>
+/// Canal de log dédié aux événements du système de types sérialisables.
+/// </summary>
 public struct HumbleTypeChannel : ILogChannel
 {
     public static string ChannelName { get; } =  "HUMBLE TYPE";
--- HumbleEngine.Core/Serialization/types/HumbleTypeRegistry.cs
+++ HumbleEngine.Core/Serialization/types/HumbleTypeRegistry.cs
@@ -2,10 +2,23 @@ using System.Reflection;
 
 namespace HumbleEngine.Core;
 
+/// <summary>
+/// Registre central des types sérialisables du moteur.
+/// Associe chaque <see cref="Guid"/> déclaré via <see cref="HumbleTypeAttribute"/> au <see cref="System.Type"/> C# correspondant.
+/// </summary>
 public class HumbleTypeRegistry
 {
     private Dictionary<Guid, Type> _types = [];
 
+    /// <summary>
+    /// Enregistre un type dans le registre.
+    /// Le type doit être décoré de <see cref="HumbleTypeAttribute"/>.
+    /// </summary>
+    /// <param name="type">Le type à enregistrer.</param>
+    /// <exception cref="InvalidOperationException">
+    /// Levée si le type n'a pas de <see cref="HumbleTypeAttribute"/>,
+    /// ou si un type avec le même identifiant est déjà enregistré.
+    /// </exception>
     public void Register(Type type)
     {
         HumbleTypeAttribute? attribute = type.GetCustomAttribute<HumbleTypeAttribute>();
@@ -23,14 +36,26 @@ public class HumbleTypeRegistry
         Services.Logger.Debug<HumbleTypeChannel>($"Type {type} has been registered.");
     }
 
+    /// <summary>
+    /// Enregistre tous les types d'une assembly qui sont décorés de <see cref="HumbleTypeAttribute"/>.
+    /// </summary>
+    /// <param name="assembly">L'assembly à scanner.</param>
     public void Register(Assembly assembly)
     {
-        foreach (Type type in assembly.GetTypes().Where(t => t.GetCustomAttribute<HumbleTypeAttribute>() != null))
+        Type[] types = assembly.GetTypes().Where(t => t.GetCustomAttribute<HumbleTypeAttribute>() != null).ToArray();
+        foreach (Type type in types)
         {
             Register(type);
         }
+        Services.Logger.Debug<HumbleTypeChannel>($"{types.Length} type(s) registered from assembly {assembly.GetName().Name}.");
     }
     
+    /// <summary>
+    /// Retourne le <see cref="System.Type"/> associé à l'identifiant donné.
+    /// </summary>
+    /// <param name="id">L'identifiant du type à résoudre.</param>
+    /// <returns>Le <see cref="System.Type"/> correspondant.</returns>
+    /// <exception cref="ArgumentException">Levée si aucun type n'est enregistré pour cet identifiant.</exception>
     public Type Resolve(Guid id)
     {
         if (!_types.TryGetValue(id, out Type? type))
