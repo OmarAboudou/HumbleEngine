0e8b938c00a0d841e532ae46638d6bd180879fd8
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-23 06:21:51 +0200

    Added Parent property to Node

--- HumbleEngine/Node.cs
+++ HumbleEngine/Node.cs
@@ -2,5 +2,8 @@ namespace HumbleEngine;
 
 public abstract class Node
 {
+    private readonly Property<Node?> _parent = new();
+    public ReadOnlyProperty<Node?> Parent => _parent.AsReadOnly();
+    
     
 }
