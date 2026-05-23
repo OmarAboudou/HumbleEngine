22d08d4d24cb33ebf280e8d218a3d7fb71314248
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-23 08:11:44 +0200

    Added a Two component Signal

--- HumbleEngine/Signal.cs
+++ HumbleEngine/Signal.cs
@@ -27,6 +27,22 @@ public class Signal<T> : IReadOnlySignal<T>
 
 }
 
+public class Signal<T1, T2> : IReadOnlySignal<T1, T2>
+{
+    internal readonly List<Action<T1, T2>> Connections = [];
+    
+    public void Connect(Action<T1, T2> callback) => Connections.Add(callback);
+    public void Disconnect(Action<T1, T2> callback) => Connections.Remove(callback);
+    
+    public void Emit(T1 arg1, T2 arg2) => Connections.ForEach(connection => connection.Invoke(arg1,arg2));
+    
+    private ReadOnlySignal<T1, T2>? _readOnlySignal;
+    public ReadOnlySignal<T1, T2> AsReadOnly() => _readOnlySignal ??= new ReadOnlySignal<T1, T2>(this);
+
+}
+
+// -----------------------------------------------------------
+
 public class ReadOnlySignal : IReadOnlySignal
 {
     private readonly Signal _signal;
@@ -47,6 +63,18 @@ public class ReadOnlySignal<T> : IReadOnlySignal<T>
     public void Disconnect(Action<T> callback) => _signal.Disconnect(callback);
 }
 
+public class ReadOnlySignal<T1, T2> : IReadOnlySignal<T1, T2>
+{
+    private readonly Signal<T1, T2> _signal;
+    
+    internal ReadOnlySignal(Signal<T1, T2> signal) => _signal = signal;
+    
+    public void Connect(Action<T1, T2> callback) => _signal.Connect(callback);
+    public void Disconnect(Action<T1, T2> callback) => _signal.Disconnect(callback);
+}
+
+// -----------------------------------------------------------
+
 public interface IReadOnlySignal
 {
     public void Connect(Action callback);
@@ -57,4 +85,10 @@ public interface IReadOnlySignal<out T>
 {
     public void Connect(Action<T> callback);
     public void Disconnect(Action<T> callback);
+}
+
+public interface IReadOnlySignal<out T1, out T2>
+{
+    public void Connect(Action<T1, T2> callback);
+    public void Disconnect(Action<T1, T2> callback);
 }
