fa9c696d0ee9e1dc5d1a3f6b91937191c25e0637
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 01:40:53 +0200

    Created a project for Silk

---
+++ HumbleEngine.Silk/Program.cs
@@ -0,0 +1,13 @@
+﻿using Silk.NET.Windowing;
+
+namespace HumbleEngine.Silk;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWindow window = Window.Create(WindowOptions.Default);
+        
+        window.Run();
+    }
+}
