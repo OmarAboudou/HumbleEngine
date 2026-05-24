9436e6a74cc3b8cfbea5749a36a962d85c8db113
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 01:58:35 +0200

    Created PropertyGetter and IPropertyGetter to allow having a read only accessor to a Property

--- HumbleEngine.Core/Property.cs
+++ HumbleEngine.Core/Property.cs
@@ -1,15 +1,23 @@
 namespace HumbleEngine.Core;
 
-public class Property<T>
+public interface IPropertyGetter<T>
+{
+    public delegate void ValueChangedHandler(T oldValue, T newValue);
+    
+    public void Connect(ValueChangedHandler handler);
+    
+    public void Disconnect(ValueChangedHandler handler);
+    
+}
+
+public class Property<T> : IPropertyGetter<T>
 {
     public Property(T initialValue)
     {
         _value = initialValue;
     }
-    
-    public delegate void ValueChangedHandler(T oldValue, T newValue);
-    public event ValueChangedHandler? ValueChanged;
-    
+
+    public IPropertyGetter<T> Getter => field ??= new PropertyGetter<T>(this);
     private T _value;
     public T Value
     {
@@ -24,7 +32,33 @@ public class Property<T>
             }
         }
     }
-    
 
-    
+    public event IPropertyGetter<T>.ValueChangedHandler? ValueChanged;
+    public void Connect(IPropertyGetter<T>.ValueChangedHandler handler)
+    {
+        ValueChanged += handler;
+    }
+
+    public void Disconnect(IPropertyGetter<T>.ValueChangedHandler handler)
+    {
+        ValueChanged -= handler;
+    }
+}
+
+internal class PropertyGetter<T> : IPropertyGetter<T>
+{
+    internal PropertyGetter(Property<T> property)
+    {
+        _property = property;
+    }
+
+    private readonly Property<T> _property;
+
+    public T Value => _property.Value;
+
+    public void Connect(IPropertyGetter<T>.ValueChangedHandler handler)
+        =>  _property.Connect(handler);
+
+    public void Disconnect(IPropertyGetter<T>.ValueChangedHandler handler)
+        =>  _property.Disconnect(handler);
 }
