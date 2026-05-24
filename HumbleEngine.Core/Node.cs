e40b25c49faa0935d082db46e743bc6800059b1a
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 00:36:20 +0200

    Added the possibility to dynamically Add or Remove and Node from its parent.

--- HumbleEngine.Core/Node.cs
+++ HumbleEngine.Core/Node.cs
@@ -5,11 +5,49 @@ public abstract class Node
     public Node? Parent { get; private set; }
     private List<Node> _children = [];
     public IReadOnlyList<Node> Children;
-
+    
     public Node()
     {
         Children = _children.AsReadOnly();
     }
     
+    public void AddChild(Node node)
+    {
+        if (node.Parent == null)
+        {
+            node.Parent = this;
+            _children.Add(node);
+            return;
+        }
+        
+        if (node == this)
+        {
+            throw new Exception($"A {nameof(Node)} cannot be its own child.");
+        }
+        else if (node.Parent == this)
+        {
+            Console.WriteLine($"{nameof(Node)}( {node} ) is already a child of {nameof(Node)}( {this} )");
+        }
+        else if (node.Parent != null)
+        {
+            throw new Exception($"{nameof(Node)}( {node} ) must have no parent when being added as a child of {nameof(Node)}( {this} )");
+        }
+    }
+
+    public void RemoveChild(Node node)
+    {
+        if (node.Parent == this)
+        {
+            _children.Remove(node);
+            node.Parent = null;
+            return;
+        }
+
+        if (node.Parent != this)
+        {
+            Console.WriteLine($"{nameof(Node)}( {node} ) cannot be removed from the children of  {nameof(Node)}( {this} ) because it is not a child of {nameof(Node)}( {this} ).");
+        }
+        
+    }
     
 }
