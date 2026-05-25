ddd941b3d2e5dc3467bf77b9981657702a3b8662
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 02:12:47 +0200

    Made Title property connected to the actual title of the window via the SetTitle abstract method

--- HumbleEngine.Core/Window.cs
+++ HumbleEngine.Core/Window.cs
@@ -2,13 +2,19 @@ namespace HumbleEngine.Core;
 
 public abstract class Window : IDisposable
 {
+    public Window()
+    {
+        Title.Connect(SetTitle);
+    }
+    
     public Property<string> Title { get; init; } = new("Humble Engine Window");
     public abstract void Initialize();
 
     public abstract void Run();
 
     public abstract void Reset();
-
+    
+    protected abstract void SetTitle(string title);
 
     public void Dispose()
     {
