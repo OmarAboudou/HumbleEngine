2bb510ff31904b8176d42f99317cff24335598be
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 07:07:09 +0200

    Implemented Signals using delegates for more comfort when using them.

--- HumbleEngine.Core/Signal.cs
+++ HumbleEngine.Core/Signal.cs
@@ -1,28 +1,35 @@
 namespace HumbleEngine.Core;
 
-public class Signal : IDisposable
+public class Signal<TDelegate>
+    where TDelegate : Delegate
 {
-    public Signal(out Action emitter)
+    private Action<TDelegate> _callbackHandler;
+    
+    public Signal(in Action<TDelegate> callbackHandler, out Action emitter)
     {
+        _callbackHandler = callbackHandler;
         emitter = Emit;
     }
 
-    private readonly List<Action> _connections = [];
+    private readonly List<TDelegate> _connections = [];
     
-    public void Connect(Action signal)
-        =>  _connections.Add(signal);
+    public void Connect(TDelegate callback)
+        =>  _connections.Add(callback);
 
-    public void Disconnect(Action signal)
-        => _connections.Remove(signal);
+    public void Disconnect(TDelegate callback)
+        => _connections.Remove(callback);
 
     private void Emit()
     {
-        foreach (var connection in _connections) 
-            connection();
+        foreach (var connection in _connections)
+            _callbackHandler(connection);
     }
-    
-    public void Dispose()
+}
+
+public class Signal : Signal<Action>
+{
+    public Signal(out Action emitter) : base((e) => e(), out Action _emitter)
     {
-        // TODO release managed resources here
+        emitter = _emitter;
     }
 }
