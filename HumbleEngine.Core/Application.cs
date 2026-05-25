62d9ad9974a5e89b8d4bc214610b89c63b959ec5
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 13:25:31 +0200

    Added Using to PlatformWindow and Window to make sure they are released properly.

--- HumbleEngine.Core/Application.cs
+++ HumbleEngine.Core/Application.cs
@@ -6,19 +6,15 @@ public abstract class Application
     protected abstract PlatformWindow CreatePlatformWindow();
     
     public void Run(ApplicationConfig config)
-    { ;
-        PlatformWindow platformWindow = CreatePlatformWindow();
-        Window window = new(platformWindow)
-        {
-            config.scene
-        };
-        platformWindow.Loaded.Connect(() =>
-        {
+    {
+        using PlatformWindow platformWindow = CreatePlatformWindow();
+        using Window window = new(platformWindow);
+        window.Add(config.scene);
+        platformWindow.Loaded.Connect(() => {
             platformWindow.FixUpdated.Connect( (delta) => Console.WriteLine($"Fix Update {1/delta}/s") );
             platformWindow.Rendering.Connect( (delta) => window.Title.Value = $"{1.0 / delta}fps" );
         });
         platformWindow.Closing.Connect( () => Console.WriteLine("CLOSING !") );
         platformWindow.Run();
-        
     }
 }
