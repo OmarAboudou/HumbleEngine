8967e23b966e82244f7bbdd5d533b634eb193f47
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-26 00:36:16 +0200

    ...

--- HumbleEngine.Core/Application Passes/FixedUpdate/FixedUpdatePass.cs
+++ HumbleEngine.Core/Application Passes/FixedUpdate/FixedUpdatePass.cs
@@ -5,11 +5,7 @@ public class FixedUpdatePass : IFixedUpdatePass
     public void Execute(Node root, double delta)
     {
         foreach (Node node in root.GetSubtreeInDepthFirstOrder())
-        {
-            if(node is IFixedUpdated fixedUpdate)
-            {
+            if(node is IFixedUpdated fixedUpdate) 
                 fixedUpdate.OnFixedUpdate(delta);
-            }
-        }
     }
 }
