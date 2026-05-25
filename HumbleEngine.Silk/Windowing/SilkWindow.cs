199a510d0b7254e099bf68a468166eb370bfd643
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2026-05-25 02:23:43 +0200

    Implemented SilkWindow

---
+++ HumbleEngine.Silk/Windowing/SilkWindow.cs
@@ -0,0 +1,31 @@
+using Silk.NET.Windowing;
+using Window = Silk.NET.Windowing.Window;
+using HumbleWindow = HumbleEngine.Core.Window;
+
+namespace HumbleEngine.Silk;
+
+public class SilkWindow : HumbleWindow
+{
+    private IWindow window;
+
+    public override void Initialize()
+    {
+        window = Window.Create(
+            WindowOptions.Default with
+            {
+                Title = Title
+            }
+        );
+    }
+
+    public override void Run() 
+        => window.Run();
+
+    public override void Reset() 
+        => window.Reset();
+
+    protected override void SetTitle(string title) 
+        => window.Title = title;
+    
+    
+}
